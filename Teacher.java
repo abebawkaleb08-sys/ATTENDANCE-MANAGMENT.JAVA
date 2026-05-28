d5a28f48728969b5238c9d49ee6bf42a2329f43c
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2026-05-28 23:03:01 +0200

    Update Teacher.java
    "Created Teacher class extending Person with constructor initialization and overridden displayInfo method for displaying teacher information."

--- Teacher.java
+++ Teacher.java
@@ -1,15 +1,14 @@
-class Teacher extends Person {
-
+public class Teacher extends Person {
+    
     public Teacher(String name, int id) {
-
         super(name, id);
     }
 
     @Override
     public void displayInfo() {
-
-        System.out.println("Teacher Information");
-
-        super.displayInfo();
+        System.out.println("Teacher Information:"); // Explains who this person is
+        super.displayInfo(); // Correctly prints Name and ID
     }
 }
+
+
