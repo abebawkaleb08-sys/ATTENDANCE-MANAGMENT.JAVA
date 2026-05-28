562e204b0effcbdb19bb04802f62383bcc131fbc
abebawkaleb08-sys <abebawkaleb08@gmail.com>
2026-05-28 23:04:38 +0200

    Update AttendanceManager.java

--- AttendanceManager.java
+++ AttendanceManager.java
@@ -1,34 +1,32 @@
 import java.util.ArrayList;
 
-class AttendanceManager {
+public class AttendanceManager {
+    private ArrayList<Student> students;
 
-    ArrayList<Student> students = new ArrayList<>();
+    public AttendanceManager() {
+        students = new ArrayList<>();
+    }
 
     public void addStudent(Student s) {
-
         students.add(s);
     }
 
-    // Method Overloading
+    // Overloaded Method 1: Mark a single day
     public void markAttendance(Student s) {
-
         s.markAttendance();
     }
 
+    // Overloaded Method 2: Mark multiple days dynamically
     public void markAttendance(Student s, int days) {
-
-        for(int i = 0; i < days; i++) {
-
+        for (int i = 0; i < days; i++) {
             s.markAttendance();
         }
     }
 
     public void showAllStudents() {
-
-        for(Student s : students) {
-
+        System.out.println("Student Records:");
+        for (Student s : students) {
             s.displayInfo();
-
             System.out.println();
         }
     }
